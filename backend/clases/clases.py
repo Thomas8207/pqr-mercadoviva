f567c908354b00aeb9d2c052b02ec99c99be21bd
¨dany <danyaleximoru@gmail.com>
2026-09-08 12:58:56 -0500

    clase administrador, cliente y aplicativo

---
+++ backend/clases/clases.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+class Aplicativo:
+    
+    def __init__(self, id: str):
+        self.id: str = id
+        self.fecha: datetime = datetime.now()
+        
+    
+    
+
+class Administrador(Aplicativo):
+    
+    def __init__(self, id):
+        super().__init__(id)
+        
+    def consultar_PQRS(self):
+        print("Consultando los PQRS")
+        
+    def supervisar_gestion(self):
+        print("Revisando y gestionando los PQR")
+    
+    def responder_cliente(self):
+        print("Respondiendo al cliente")
+        
+
+class Cliente(Aplicativo):
+    
+    def __init__(self, id):
+        super().__init__(id)
+    
+    def registar_PQR(self):
+        print("Registrando PQRS")
+    
+    
+    def consultar_estados(self):
+        print("Consultando Estados del PQR")
