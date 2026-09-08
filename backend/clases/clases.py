a8968a2bbe2f7e85be4a590b895fdcd96fd7ecb2
¨dany <danyaleximoru@gmail.com>
2026-09-08 13:07:35 -0500

    Agregue la clase administrador, cliente y aplicativo

--- backend/clases/clases.py
+++ backend/clases/clases.py
@@ -1,36 +1,35 @@
 from datetime import datetime
+
+
 class Aplicativo:
-    
+
     def __init__(self, id: str):
         self.id: str = id
         self.fecha: datetime = datetime.now()
-        
-    
-    
+
 
 class Administrador(Aplicativo):
-    
+
     def __init__(self, id):
         super().__init__(id)
-        
+
     def consultar_PQRS(self):
-        print("Consultando los PQRS")
-        
+        print("Consultando los PQRS..")
+
     def supervisar_gestion(self):
-        print("Revisando y gestionando los PQR")
-    
+        print("Gestionando PQR")
+
     def responder_cliente(self):
-        print("Respondiendo al cliente")
-        
+        print("Respondiendo al cliente...")
+
 
 class Cliente(Aplicativo):
-    
+
     def __init__(self, id):
         super().__init__(id)
-    
+
     def registar_PQR(self):
-        print("Registrando PQRS")
-    
-    
+        print("Registrando PQRS..")
+
     def consultar_estados(self):
-        print("Consultando Estados del PQR")
+        print("Consultando Estados del PQR...")
